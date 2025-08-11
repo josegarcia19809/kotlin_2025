d1328f703aec83b31b7b3718fe36030660315ac0
josegarcia19809 <josegarcia19809@gmail.com>
2025-08-11 07:34:54 -0600

    Actualización

---
+++ src/p01_fundamentos/TiendaLibros.kt
@@ -0,0 +1,17 @@
+package p01_fundamentos
+
+fun main() {
+    println("-".repeat(100))
+    println("Tienda de libros")
+
+    print("Proporciona el título: ")
+    val nombre = readln()
+
+    print("Proporciona el id: ")
+    val id: Int = readln().toInt();
+
+    print("Proporciona el precio: ")
+    val precio: Double= readln().toDouble()
+
+    
+}
