6cf11cef5fe7269a376e6e8646a749aecd988c2b
josegarcia19809 <josegarcia19809@gmail.com>
2025-08-11 07:55:35 -0600

    Tipos de datos: Tienda de libros

--- src/p01_fundamentos/TiendaLibros.kt
+++ src/p01_fundamentos/TiendaLibros.kt
@@ -11,7 +11,16 @@ fun main() {
     val id: Int = readln().toInt();
 
     print("Proporciona el precio: ")
-    val precio: Double= readln().toDouble()
+    val precio: Double = readln().toDouble()
 
-    
+    print("Proporciona el simbolo del precio: ")
+    val simbolo: Char = readln()[0]
+
+    print("Proporciona si es envío gratuito (true/false): ")
+    val esEnvioGratuito: Boolean = readln().toBoolean()
+
+    println()
+    println("$nombre #$id")
+    println("Precio: $simbolo$precio")
+    println("Es envío gratuito: $esEnvioGratuito")
 }
