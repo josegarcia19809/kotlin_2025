4df9a5c1cff7931626945417377cbcc8448eec5b
Jose Garcia <josegarcia19809@gmail.com>
2025-11-25 17:13:20 -0600

    POO: interface Pet

---
+++ src/p05_poo/InterfacePet.kt
@@ -0,0 +1,45 @@
+package p05_poo
+
+interface Pet {
+    var cutenessLevel: Int
+    fun startPlaying()
+    fun feed()
+}
+
+class Puppy : Pet {
+    override var cutenessLevel = 5
+    override fun startPlaying() {
+        println("Dogs play by running, jumping and barking")
+    }
+
+    override fun feed() {
+        println("Give doggy treats")
+    }
+}
+
+class Child {
+    val pet: Pet
+
+    init {
+        val petStore = PetStore()
+        pet = petStore.getPet()
+    }
+
+    fun raisePet() {
+        for (i in 1..pet.cutenessLevel) {
+            pet.startPlaying()
+        }
+        pet.feed()
+    }
+}
+
+class PetStore {
+    fun getPet(): Pet {
+        return Puppy()
+    }
+}
+
+fun main() {
+    val child = Child()
+    child.raisePet()
+}
