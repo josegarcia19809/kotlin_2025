efd47eb196b5bf88258de742ed2e178b64a4bc32
josegarcia19809 <josegarcia19809@gmail.com>
2025-11-27 20:54:11 -0600

    Standard Functions: let

---
+++ src/p06_funciones_standard/let_animal.kt
@@ -0,0 +1,24 @@
+package p06_funciones_standard
+
+import p04_funciones.imprimirLinea
+
+
+fun main() {
+    val animals = listOf("cat", "dog", "mouse", "bear", "zebra")
+
+    animals.map { it.length }
+        .filter { it > 3 }
+        .let {
+            println(it)
+            println("Size of list is ${it.size}")
+        }
+
+    imprimirLinea()
+
+    animals.map { it.length }
+        .filter { it <= 4 }
+        .let { filteredList ->
+            println(filteredList)
+            println("Size of list is ${filteredList.size}")
+        }
+}
